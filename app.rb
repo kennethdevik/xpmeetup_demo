09268cc77263f4278a320d2e05d4a8dcf5b24528
Kenneth Devik <kenneth@devik.net>
2012-02-06 21:46:44 +0100

    added post route to add members

--- app.rb
+++ app.rb
@@ -5,3 +5,13 @@ require './members'
 get '/' do
   'Hello world!'
 end
+
+post '/member/add' do
+  member = Members.new(params)
+
+  if member.save
+    "Added #{member.name} with e-mail #{member.email} and id #{member.id}"
+  else
+    "Adding failed"
+  end
+end
